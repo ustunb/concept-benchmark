a68df3f9024a7f26bd426676adecfa04a47f654a
Shreyas Kadekodi <61361442+Kadekool@users.noreply.github.com>
2025-10-15 14:53:59 -0700

    Remove generic rate and tolerance arguments
    Removed arguments for generic rate and tolerance from the script.

--- scripts/run_robot_demos.py
+++ scripts/run_robot_demos.py
@@ -125,8 +125,6 @@ def parse_cli():
     ap.add_argument("--lf-device", type=str)
     ap.add_argument("--lf-batch-size", type=int)
     ap.add_argument("--dev-size", type=int)
-    ap.add_argument("--generic-rate", "--generic_rate", dest="generic_rate", type=float)
-    ap.add_argument("--generic-tol", "--generic_tol", dest="generic_tol", type=float)
     ap.add_argument("--val-balance-enable", "--val_balance_enable", dest="val_balance_enable", type=int)
     ap.add_argument("--test-balance-enable", "--test_balance_enable", dest="test_balance_enable", type=int)
     ap.add_argument("--policy", type=str)
